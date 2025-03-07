e7db5de92d826b7bd42eece65b9923db8c3ee87a
Mario Angelino Uribe <Mario_Angelino@hotmail.com>
2025-03-06 18:03:14 -0600

    #catch 404

--- routes/index.js
+++ routes/index.js
@@ -9,36 +9,77 @@ import { CryptoHandlerO } from '../CryptHandlerMaestro.js';
 const router = express.Router();
 
 router.get('/', (req,res)=>{
-   res.status(200).send('ok, 1.0002');
+   res.status(200).send('ok, 1.0005');
 })
 
 //---------------------------------------------------ENDPOINTS AL STP DIRECTO, TIENE IP CAMBIANTE-------------------------------------------------------------------------------
-router.post('/registraOrden', (req,res)=>{
-  res.status(200).send('Ok');
-  console.log(req.body);
-axios({
-            url: 'https://bond-app-1.bubbleapps.io/api/1.1/wf/pagoabond',
-            method: 'post',
-            responseType: 'json',
-            data: req.body,
-            headers: {'Content-Type':'application/json'},
-            rejectUnauthorized: false 
-          });
+router.post('/registraOrden', async (req,res)=>{
+  console.log('Datos recibidos: ', req.body);
+  try {
+    const response = await axios({
+      url: 'https://bond-app-1.bubbleapps.io/api/1.1/wf/pagoabond',
+      method: 'post',
+      responseType: 'json',
+      data: req.body,
+      headers: {'Content-Type':'application/json'},
+      rejectUnauthorized: false 
+    });
+    
+    console.log("Respuesta API: ", response.data);
+    res.status(200).send('Ok');
 
-          
+  } catch (error) {
+    console.log("Error: ", error.message);
+    if (error.response) {
+      console.error('Código de estado:', error.response.status);
+      console.error('Cuerpo del error:', error.response.data);
+      res.status(error.response.status).json({ 
+        error: 'Error en la API externa', 
+        detalles: error.response.data 
+      });
+    } else if (error.request) {
+      console.error('No hubo respuesta de la API externa:', error.request);
+      res.status(500).json({ error: 'No hubo respuesta de la API externa' });
+    } else {
+      console.error('Error al configurar la solicitud:', error.message);
+      res.status(500).json({ error: 'Error interno en el servidor' });
+    }
+  }          
 });
 
-router.post('/cambioEstado', (req,res)=>{
+router.post('/cambioEstado', async (req,res)=>{
+  console.log('Datos recibidos: ', req.body);
   res.status(200).send('Ok');
-  console.log(req.body);
-  axios({
-    url: 'https://bond-app-1.bubbleapps.io/api/1.1/wf/cambiodeestado',
-    method: 'post',
-    responseType: 'json',
-    data: req.body,
-    headers: {'Content-Type':'application/json'},
-    rejectUnauthorized: false
-  });
+  try {
+    const response = await axios({
+      url: 'https://bond-app-1.bubbleapps.io/api/1.1/wf/cambiodeestado',
+      method: 'post',
+      responseType: 'json',
+      data: req.body,
+      headers: {'Content-Type':'application/json'},
+      rejectUnauthorized: false
+    });
+
+    console.log("Respuesta API: ", response.data);
+    res.status(200).send('Ok');
+
+  } catch (error) {
+    console.log("Error: ", error.message);
+    if (error.response) {
+      console.error('Código de estado:', error.response.status);
+      console.error('Cuerpo del error:', error.response.data);
+      res.status(error.response.status).json({ 
+        error: 'Error en la API externa', 
+        detalles: error.response.data 
+      });
+    } else if (error.request) {
+      console.error('No hubo respuesta de la API externa:', error.request);
+      res.status(500).json({ error: 'No hubo respuesta de la API externa' });
+    } else {
+      console.error('Error al configurar la solicitud:', error.message);
+      res.status(500).json({ error: 'Error interno en el servidor' });
+    }
+  }
 
 });
 
